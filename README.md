3c5125745b9e1ab5a189b4b31d9c978324ff4032
Ignasi Llopis <iglloala@gmail.com>
2018-01-10 23:11:05 +0100

    Primera funcionalidad AJAX
    Se ha conseguido obtener y mostrar información de la API a través de AJAX haciendo uso de las funciones de jQuery. Lo siguiente será implementar handlebars

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Fecha creación: Enero 2018
 
 // DESCRIPCIÓN
 
-Se nos pide realizar una página en la que haciendo uso de Jquey hagamos peticiones AJAX a alguna API pública y mediante Handlebars.js mostremos la información obtenida dándole un formato bonito con boostrap.
+Se nos pide realizar una página en la que haciendo uso de jQuery hagamos peticiones AJAX a alguna API pública y mediante Handlebars.js mostremos la información obtenida dándole un formato bonito con boostrap.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
 /* Mi código js */
+var miCodigo = (function(){
+	// Propiedades
+	var _bloqueResultado = $('#resultado');
+	var _loader = $('#loader');
+
+	// Método init que asocia los eventos
+	function _init(){
+		var btLogrosPVE = $('#btLogrosPVE');
+		btLogrosPVE.on('click', _cargarLogrosPVE);
+	}
+	_init();
+
+	// Métodos
+	function _cargarLogrosPVE(){
+		// Muestro el icono de carga
+		_loader.css('visibility', 'visible');
+		// Inicio una petición AJAX para recuperar los id's de los logros diarios pve
+		var jqxhr = $.get("https://api.guildwars2.com/v2/achievements/daily");
+		// done
+		jqxhr.done(function(datos){
+			// Extraigo los id's
+			var listaIds = datos.pve.map(function(logro){
+				return logro.id;
+			});
+			// Preparo los parámetros para una nueva petición
+			var parametros = {lang: 'es', ids: listaIds.join()};
+			// Inicio otra petición AJAX para recuperar los datos definitivos
+			var jqxhr = $.get("https://api.guildwars2.com/v2/achievements", parametros);
+			jqxhr.done(function(datos){
+				// Caundo ya tengo toda la info de los logros diarios pve los muestro en la capa de resultado
+				datos.forEach(function(logro){
+					var titulo = $('<h6>').append(logro.name);
+					var requisito = $('<p>').append(logro.requirement);
+					_bloqueResultado.append(titulo);
+					_bloqueResultado.append(requisito);
+				})
+			})
+			// Oculto el icono de carga
+			_loader.css('visibility', 'hidden');
+		});
+		// fail
+		jqxhr.fail(function(){
+			// Oculto el icono de carga
+			_loader.css('visibility', 'hidden');
+			// Muestro mensaje de error
+			_bloqueResultado.html($("<p>No se ha podido recuperar ningún dato</p>"));
+		});
+		// always
+		jqxhr.always(function(){
+			// Vacío la capa de resultados
+			_bloqueResultado.html("");
+		})
+	}
+
+	return {
+		cargarLogrosPVE: _cargarLogrosPVE
+	}
+})();
